59a30b1e8d6d4f8bc9339f4c1cbd672d4c71ad23
Blade Nelson <powderblock@gmail.com>
2015-07-03 19:48:56 -0400

    Add input file, add file saving
    In markdown_to_HTML.py:
    -Add sys args.
    -Saving input md as output .html.

--- markdown_to_HTML.py
+++ markdown_to_HTML.py
@@ -1,48 +1,55 @@
+# Used for string.replace()
 import string
-while(1):
-    # Take input from the user:
-    md = str(raw_input("Enter some markdown: "))
+import sys
 
-    # Have we found bold or italic characters yet?
-    bold = False
-    italics = False
-    # No. That's why these are false.
+# Take input from the user:
+with open(sys.argv[1], 'r') as content_file:
+    md = content_file.read()
 
-    # Replace some characters so the string is easier to work with:
-    md = string.replace(md, "***", "*_")
-    md = string.replace(md, "**", "_")
-    print md
+# Have we found bold or italic characters yet?
+bold = False
+italics = False
+# No. That's why these are false.
 
-    # Change the user input into a list:
-    html = list(md)
+# Replace some characters so the string is easier to work with:
+md = string.replace(md, "***", "*_")
+md = string.replace(md, "**", "_")
+print md
 
-    for letter in  range(len(html)):
-    # Bold code:
-            if html[letter] == "_" and bold == False:
-                html[letter] = "<strong>"
-                # Awesome! We found a bold character! Switch that baby to true!
-                bold = True
+# Change the user input into a list:
+html = list(md)
 
-            if html[letter] == "_" and bold == True:
+for letter in  range(len(html)):
+# Bold code:
+        if html[letter] == "_" and bold == False:
+            html[letter] = "<strong>"
+            # Awesome! We found a bold character! Switch that baby to true!
+            bold = True
+
+        if html[letter] == "_" and bold == True:
+            html[letter] = "</strong>"
+            # That's all, folks!
+            bold = False
+
+# Italics code:
+        if html[letter] == "*" and italics == False:
+            html[letter] = "<em>"
+            # Italic character found, sweet! Make sure our bool knows it.
+            italics = True
+
+        if html[letter] == "*" and italics == True:
+            # If the user did ***, switch current order so it ends </strong> </em>
+            if html[letter + 1] == "_":
                 html[letter] = "</strong>"
-                # That's all, folks!
-                bold = False
+                html[letter + 1] = "</em>"
+            else:
+                html[letter] = "</em>"
+            # No more italics for you, that's a shame. (End of it found.)
+            italics = False
 
-    # Italics code:
-            if html[letter] == "*" and italics == False:
-                html[letter] = "<em>"
-                # Italic character found, sweet! Make sure our bool knows it.
-                italics = True
+html = "".join(html)
+print html
 
-            if html[letter] == "*" and italics == True:
-                # If the user did ***, switch current order so it ends </strong> </em>
-                if html[letter + 1] == "_":
-                    html[letter] = "</strong>"
-                    html[letter + 1] = "</em>"
-                else:
-                    html[letter] = "</em>"
-                # No more italics for you, that's a shame. (End of it found.)
-                italics = False
-
-    html = "".join(html)
-    print html
+text_file = open(sys.argv[1].split(".")[0]+".html", "w")
+text_file.write(html)
+text_file.close()
